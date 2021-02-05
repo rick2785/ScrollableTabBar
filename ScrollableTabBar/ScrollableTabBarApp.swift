99528edcfda0a2ce85d00bccd3fd8aeb11dc5e71
rick2785 <rjhrabowskie@gmail.com>
2021-02-05 08:05:11 -0600

    Initial Commit

---
+++ ScrollableTabBar/ScrollableTabBarApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScrollableTabBarApp.swift
+//  ScrollableTabBar
+//
+//  Created by RJ Hrabowskie on 2/5/21.
+//
+
+import SwiftUI
+
+@main
+struct ScrollableTabBarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
